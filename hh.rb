c0169af52c35c3c714b59d7a9d4fc88ea76a1908
Warm <warm@intropica,com>
2013-01-18 11:18:47 +0700

    caching proxy

--- hh.rb
+++ hh.rb
@@ -1,11 +1,28 @@
 require 'sinatra'
 require "HTTParty"
 require 'json'
+require "redis"
+require 'date'
 
 set :public_folder, '.'
 
+REDIS = Redis.new
+EXPIRES = 60 * 60 * 24 * 3 # 3 days
+
 get '/proxy' do
-  response = HTTParty.get(params[:url])
   content_type :json
-  { :url => params[:url], :body => response.body}.to_json
+  { :url => params[:url], :body => fetch_data(params[:url])}.to_json
+end
+
+def fetch_data(url)
+  body = REDIS.get(url)
+  body = get_new_data(url) if body.nil?
+  body
+end
+
+def get_new_data(url)
+  response = HTTParty.get(url)
+  REDIS.set(url,response.body)
+  REDIS.expire(url, EXPIRES)
+  response
 end
