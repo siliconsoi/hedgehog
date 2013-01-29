701ecd578bb4fa9f7fec6305e46022e122015c2b
tour <tour@intropica.com>
2013-01-29 17:08:46 +0700

    dasdasd

--- javascript/document_handlers.js
+++ javascript/document_handlers.js
@@ -1,73 +1,107 @@
 var translators = (function(){
 
 	var translators = [{
+		id: 'page_title',
 		urlRegex: /.*/,
-		decoder: handler(text_extractor('page_title', /<title[\s\S]*<\/title>/i))
+		decoder: handler(text_extractor(/<title[\s\S]*<\/title>/i))
 	}, {
+		id: 'hotel_name',
 		urlRegex: /^.+agoda.*.+$/,
-		decoder: handler(text_extractor('hotel_name', /<h1 *itemprop="name">[\s\S]*<\/h1>/i))
+		decoder: handler(text_extractor(/<h1 *itemprop="name">[\s\S]*<\/h1>/i))
 	}, {
+		id: 'agoda_breadcrumb',
 		urlRegex: /^.+agoda.*.+$/,
-		decoder: handler(agoda_breadcrumb)
+		decoder: handler(breadcrumb(/<a id="[0-9a-z_]+breadcrumbLink"[\s\S]*?<\/a>/gi))
 	}, {
+		id: 'hotel_name',
 		urlRegex: /^.+booking.com.+$/,
-		decoder: handler(text_extractor('hotel_name', /<h1 *class="item">[\s\S]*<\/h1>/i))
+		decoder: handler(text_extractor(/<h1 *class="item">[\s\S]*<\/h1>/i))
 	}, {
+		id: 'hotel_name',
 		urlRegex: /^.+tripadvisor.co.+\/Hotel_Review.+$/,
-		decoder: handler(text_extractor('hotel_name', /<h1 *id="HEADING" *property="v:name">[\s\S]*<\/h1>/i))
+		decoder: handler(text_extractor(/<h1 *id="HEADING" *property="v:name">[\s\S]*<\/h1>/i))
 	}, {
+		id: 'trip_advisor_breadcrumb',
 		urlRegex: /^.+tripadvisor.co.+\/Hotel_Review.+$/,
-		decoder: handler(tripad_splen_breadcrumb)
+		decoder: handler(breadcrumb(/<div *itemscope itemtype=.*>[\s\S]*?<\/div>/g))
 	}, {
+		id: 'hotel_name',
 		urlRegex: /^.+expedia.co.+\/.+$/,
-		decoder: handler(text_extractor('hotel_name', /<h1 *id="address-hotel-name">[\s\S]*?<\/h1>/i))
+		decoder: handler(text_extractor(/<h1 *id="address-hotel-name">[\s\S]*?<\/h1>/i))
 	}, {
+		id: 'expedia_breadcrump',
 		urlRegex: /^.+expedia.co.+\/.+$/,
-		decoder: handler(expedia_breadcrump)
+		decoder: handler(breadcrumb(/<span *typeof="v:Breadcrumb">[\s\S]*?<\/span>/g))
 	}, {
+		id: 'hotel_name',
 		urlRegex: /^.+hotels.co.+\/hotel.+$/,
-		decoder: handler(text_extractor('hotel_name', /<h1 *class="fn org">[\s\S].*<\/h1>/i))
+		decoder: handler(text_extractor(/<h1 *class="fn org">[\s\S].*<\/h1>/i))
 	}, {
+		id: 'hotel_name',
 		urlRegex: /^.+splendia.com\/.+$/,
-		decoder: handler(text_extractor('hotel_name', /<span *itemprop="name">[\s\S].*<\/span>/i))
+		decoder: handler(text_extractor(/<span *itemprop="name">[\s\S].*<\/span>/i))
 	}, {
+		id: 'splendia_breadcrumb',
 		urlRegex: /^.+splendia.com\/.+$/,
-		decoder: handler(tripad_splen_breadcrumb)
+		decoder: handler(breadcrumb(/<div *itemscope itemtype=.*>[\s\S]*?<\/div>/g))
 	}, {
+		id: 'hotel_name',
 		urlRegex: /^.+wotif.com+\/hotel.+$/,
-		decoder: handler(text_extractor('hotel_name', /<h1 *class="section">[\s\S].*<\/h1>/i))
+		decoder: handler(text_extractor(/<h1 *class="section">[\s\S].*<\/h1>/i))
 	}, {
+		id: 'wotif_breadcrumb',
 		urlRegex: /^.+wotif.com+\/hotel.+$/,
-		decoder: handler(wotif_breadcrump)
+		decoder: handler(breadcrumb(/<li *class="crumb-[a-z0-9_-]+">[\s\S] +< *[a-z]+ *[^>]+>[^<]+<\/[a-z]+>[\s\S] +<\/li>/gi))
 	}, {
+		id: 'open_graph',
 		urlRegex: /.*/,
 		decoder: handler(open_graph)
 	}, {
+		id: 'canonical',
 		urlRegex: /.*/,
 		decoder: handler(canonical)
 	}, {
+		id: 'meta_tags',
 		urlRegex: /.*/,
 		decoder: handler(meta_tags)
+	},{
+		id: 'given_url',
+		urlRegex: /.*/,
+		decoder: handler(given_url)
+	},{
+		id: 'micro_data',
+		urlRegex: /.*/,
+		decoder: handler(micro_data)
 	}];
 
 	function handler(fn) {
-		return function(body) {
-			console.log('applying ' + fn.name);
-			return $.extend({}, fn(body), {extractor: fn.name});
+		return function(body, context) {
+			return $.extend({}, fn(body, context), {extractor: context.id});
 		};
 	}
 
-	function text_extractor(name, regex) {
-		return function(body) {
+	function text_extractor(regex) {
+		return function (body, context) {
 			var element =  body.match(regex),
 				result = {data: {}};
 			if( element === null ) { return {data:{}}; }
-			result.data[name] = $(element[0]).text().trim();
+			result.data[context.id] = $(element[0]).text().trim();
 			return result;
 		};
 	}
 
-	function meta_tags(body) {
+	function breadcrumb (regex) {
+		return function(body, context) {
+			var element = body.match(regex),
+				result = {data: {}};
+			result.data[context.id] = [];
+			if( element === null ) { return {data:{}}; }
+			$.each(element, function(i, e){ result.data[context.id].push($(e).text().trim()); });
+			return result;
+		};
+	}
+
+	function meta_tags(body, context) {
 		var data = body.match(/<meta *name="[a-z_-]+" *content=+"[^"]+" *\/>/g),
 			collector = {data: {meta_tags: {}}};
 		if( data === null ) { return {data: {}}; }
@@ -80,7 +114,7 @@ var translators = (function(){
 		return collector;
 	}
 
-	function open_graph(body) {
+	function open_graph(body, context) {
 		var data = body.match(/<meta *property="og:[a-z_-]+" *content=+"[^"]+" *[^\/]*\/>/g),
 			collector = {data: {open_graph: {}}};
 			if( data === null ) { return {data: {}}; }
@@ -93,38 +127,19 @@ var translators = (function(){
 		return collector;
 	}
 
-	function canonical(body) {
+	function canonical(body, context) {
 		var canonical_url = body.match(/<link rel="canonical" href="(.+?)"/i);
 		if( canonical_url === null ) { return {data: {}}; }
 		return {data: {canonical_url: canonical_url[1]}};
 	}
 
-	function agoda_breadcrumb (body) {
-		var crumbs = body.match(/<a id="[0-9a-z_]+breadcrumbLink"[\s\S]*?<\/a>/gi),
-			result = {data: {agoda_breadcrumb: []}};
-		$.each(crumbs, function(i, e){ result.data.agoda_breadcrumb.push($(e).text()); });
-		return result;
+	function given_url(body, context) {
+		return {data: {given_url: context.url} };
 	}
 
-	function wotif_breadcrump (body) {
-		var crumbs = body.match(/<li *class="crumb-[a-z0-9_-]+">[\s\S] +< *[a-z]+ *[^>]+>[^<]+<\/[a-z]+>[\s\S] +<\/li>/gi),
-			result = {data: {wotif_breadcrumb: []}};
-		$.each(crumbs, function(i, e){ result.data.wotif_breadcrumb.push($(e).text().trim()); });
-		return result;
-	}
-
-	function tripad_splen_breadcrumb (body) {
-		var crumbs = body.match(/<div *itemscope itemtype=.*>[\s\S]*?<\/div>/g),
-			result = {data: {trip_advisor_breadcrumb: []}};
-		$.each(crumbs, function(i, e){ result.data.trip_advisor_breadcrumb.push($(e).text().trim()); });
-		return result;
-	}
-
-	function expedia_breadcrump (body) {
-		var crumbs = body.match(/<span *typeof="v:Breadcrumb">[\s\S]*?<\/span>/g),
-			result = {data: {expedia_breadcrumb: []}};
-		$.each(crumbs, function(i, e){ result.data.expedia_breadcrumb.push($(e).text().trim()); });
-		return result;
+	function micro_data (body, context) {
+		// requires asynchronicity
+		return {data: {}};
 	}
 
 	return translators;
